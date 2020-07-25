ae6e52794c1f56643b0ac01fb267eb3b91318f60
aakarshitpandey <aakarshit.pandey7@gmail.com>
2020-07-26 03:46:40 +0530

    Updates

---
+++ precache-manifest.a4698f0577e6e25d55da61995413400c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4997ced263c20eb460ddbe2c37ec1b87",
+    "url": "/auscultatorySoundAnalysis/index.html"
+  },
+  {
+    "revision": "db7771af8efff3d5975b",
+    "url": "/auscultatorySoundAnalysis/static/css/main.6333ec6f.chunk.css"
+  },
+  {
+    "revision": "849bf94e0fc0b14291ad",
+    "url": "/auscultatorySoundAnalysis/static/js/2.79205257.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/auscultatorySoundAnalysis/static/js/2.79205257.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "db7771af8efff3d5975b",
+    "url": "/auscultatorySoundAnalysis/static/js/main.fdd4ced9.chunk.js"
+  },
+  {
+    "revision": "b329cbff786dde1e6ebf",
+    "url": "/auscultatorySoundAnalysis/static/js/runtime-main.f10e0cff.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/auscultatorySoundAnalysis/static/media/logo.5d5d9eef.svg"
+  }
+]);
